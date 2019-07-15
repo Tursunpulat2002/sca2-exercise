2de59fa31ce52a9623384efe327f740d0f79625f
Tursunpulat <Tursunpulat2002@gmail.com>
2019-07-15 16:44:05 -0700

    Finished filter and map

--- 1_PM/filter.js
+++ 1_PM/filter.js
@@ -5,10 +5,19 @@ const arr = [7, 8, 15, 14, 13, 70, 44, 35, 88, 77, false, "7", "hi", null];
 
 const multOf7 = el => {
   // TODO
+  if(el % 7 != 0){
+    return false
+  }else if (typeof(el) !== 'number') {
+    return false
+  }else{
+    return true
+  }
 };
 
 Array.prototype.filter = function(fun) {
   // TODO
+  const arr = this
+  return fun(arr)
 };
 
 const newArr = arr.filter(multOf7);
--- 1_PM/map.js
+++ 1_PM/map.js
@@ -1,10 +1,19 @@
 // Complete the implementation of the following functions so that
 // the output is [0, 3, 6, 9, 12]
 
-const arr = [0, 1, 2, 3, 4];
+const arr = [0, 1, 2, 3, 4, 5];
 
 const triple = () => {
   // TODO
+  arr2 = []
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == 0){
+      arr2.push(0)
+    }else{
+      arr2.push(arr[i] * 3)
+    }
+  }
+  return arr2
 };
 
 Array.prototype.map = function(fun) {
